34964fb2f1b055c3ed4fee639b7157482e9aa628
Ralgert <Ralgert@users.noreply.github.com>
2026-01-14 22:32:33 +0100

    add namespace
    Updated version number and improved description for clarity.

--- Diplomatie/HoG_Diplomacy_CSS_Fix.user.js
+++ Diplomatie/HoG_Diplomacy_CSS_Fix.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name        HoG Diplomacy CSS Fix
+// @namespace   https://github.com/Ralgert/HoG
 // @match       *://*.konggames.com/gamez/*
 // @match       *://*.heartofgalaxy.com/*
 // @grant       GM_addStyle
-// @version     0.2.4
+// @version     0.2.6
 // @author      Ralgert / Gemini
 // @description 20260114
 // @description Für das Spiel Heart of Galaxy: Horizons
-// @description auch bei richtigen Voraussetzungen bleibt der Menü Punkt ausgeblendet; hiermit nicht
+// @description auch bei richtigen Voraussetzungen bleibt der Menü Punkt Diplomatie ausgeblendet; hiermit nicht
 // ==/UserScript==
 
 GM_addStyle('#b_diplomacy_icon { display: inline-block !important; cursor: pointer !important; }');
