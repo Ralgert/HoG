9e92edc5cf846bf04e52e894fd7d772a3f251ed3
Ralgert <Ralgert@users.noreply.github.com>
2026-01-14 22:57:31 +0100

    add @downloadURL und @updateURL

--- Diplomatie/HoG_Diplomacy_CSS_Fix.user.js
+++ Diplomatie/HoG_Diplomacy_CSS_Fix.user.js
@@ -4,11 +4,13 @@
 // @match       *://*.konggames.com/gamez/*
 // @match       *://*.heartofgalaxy.com/*
 // @grant       GM_addStyle
-// @version     0.2.6
+// @version     0.2.7
 // @author      Ralgert / Gemini
 // @description 20260114
 // @description Für das Spiel Heart of Galaxy: Horizons
 // @description auch bei richtigen Voraussetzungen bleibt der Menü Punkt Diplomatie ausgeblendet; hiermit nicht
+// @downloadURL https://raw.githubusercontent.com/Ralgert/HoG/main/Diplomatie/HoG_Diplomacy_CSS_Fix.user.js
+// @updateURL   https://raw.githubusercontent.com/Ralgert/HoG/main/Diplomatie/HoG_Diplomacy_CSS_Fix.user.js
 // ==/UserScript==
 
 GM_addStyle('#b_diplomacy_icon { display: inline-block !important; cursor: pointer !important; }');
